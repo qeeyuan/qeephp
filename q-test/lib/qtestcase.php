7b6bd4bbc303d29e502c8180e7de7d167520ccdd
YuLei Liao <dualface@gmail.com>
2009-03-27 22:16:35 +0800

    * 添加 q-test

---
+++ q-test/lib/qtestcase.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+
+abstract class QTestCase extends PHPUnit_Framework_TestCase
+{
+
+}
+
---
+++ q-test/lib/qtestsuite.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+
+abstract class QTestSuite extends PHPUnit_Framework_TestSuite
+{
+
+}
+
